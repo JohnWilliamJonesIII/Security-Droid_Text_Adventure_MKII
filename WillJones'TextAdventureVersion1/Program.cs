20b7be2034f9aceb9bb8f5f2ae1d8f1c68309457
John <johnwilliamjonesiii@gmail.com>
2019-06-20 13:42:19 -0500

    Version 2

---
+++ WillJones'TextAdventureVersion1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace WillJones_TextAdventureVersion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You wake up and find yourself in a dreary gray building. You wonder where you actually are, and why you woke up here.");
+            Console.WriteLine("You pick yourself up off of the floor, and you notice the floor is metal. Where am I?, you think to yourself");
+            Console.WriteLine("Suddenly, an extremely loud blaring noise snaps you back to reality.");
+            Console.WriteLine("WARNING");
+            Console.WriteLine("WARNING");
+            Console.WriteLine("WARNING");
+            Console.WriteLine("UNAUTHORIZED USER HAS BREACHED CONTAINMENT");
+            Console.WriteLine("PROVIDE AUTHORIZATION OR FACE JUDICIAL PUNISHMENT");
+            Console.WriteLine("A small droid scuttles out from underneath a hidden panel.");
+            Console.WriteLine("With an aggressive whizz and a threatening hum, the robot asks in a surprisingly commanding tone:");
+            Console.WriteLine("INTRUDER, DO YOU POSSESS A VISA FOR SECURITY ACCESS IN THIS AREA?");
+            string response = Console.ReadLine();
+
+            if (response == "Yes")
+            {
+                Console.WriteLine("PLEASE SWIPE YOUR SECURITY PASS ABOVE THE RED LINE PLEASE.");
+                Console.WriteLine("IF YOU DO NOT COMPLY, LETHAL FORCE IS AUTHORIZED AND WILL BE ACTIVATED");
+                Console.WriteLine("You notice that the little robot seems to be overheating, as there are little black puffs emanating from beneath its plastic shell. Perhaps it's malfunctioning?");
+                Console.WriteLine("DO YOU HAVE YOUR SECURITY PASS, ANSWER YES OR NO INTRUDER.");
+                response = Console.ReadLine();
+                if (response == "Yes")
+                {
+                Console.WriteLine("ERRORMACRO, PLEASE SWIPE CARD AGAIN MA'AM");
+                Console.WriteLine("The robot suddenly flashes its photoreceptors in hues of red and blue.");
+                Console.WriteLine("ERRORERRORERRORERRORERRORERROR CALLING FOR BACKUP ERRORERRORERRORERRORERRORERRORERROR");
+                Console.WriteLine("The robot suddenly pops its head back, revealing a large hidden weapon.");
+                Console.WriteLine("CRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKACRACKA");
+                Console.WriteLine("You slump over, riddled with flechette shavings. You should have had your security pass.");
+                Console.WriteLine("Congraulations, you've lost.");
+                }
+
+
+            }
+            if (response == "No")
+            {
+                Console.WriteLine("THANK YOU SIR. CAN I HELP YOU IN ANY OTHER FASHION?");
+                response = Console.ReadLine();
+                if (response == "Yes")
+                {
+                Console.WriteLine("EXCELLENT. FOLLOW ME SIR, AND I WILL ESCORT YOU TO CUSTOMS TO RECEIVE A COMPLEMENTARY SUBWAY COOKIE, COURTESY OF SUBWAY: EAT FRESH.");
+                Console.WriteLine("The droid begins to roll away, but suddenly it begins to fume with black smoke. It fizzles, and with a loud POP, explodes into tiny pieces.");
+                Console.WriteLine("Do you walk towards the broken robot?");
+                }
+                if (response == "No")
+                {
+                Console.WriteLine("THANK YOU VISITOR, IF YOU REQUIRE ANY FUTHER ASSISTANCE, PLEASE BE SURE TO CONTACT MARGE AT CUSTOMS.");
+                Console.WriteLine("The robot suddenly sputters, and before you can react, the droid shakes, and the last thing you see before your vision  turns dark is the bright flash of the droid's gun barrels.");
+                Console.WriteLine("Congratulations, you've lost.");
+                }
+
+                response = Console.ReadLine();
+                if (response == "Yes")
+                {
+                Console.WriteLine("Just as you crouch down to get a good look at the robot, suddenly the little pile of smoking metal and plastic that once asked for your security code again violently explodes, sending bits of white-hot metal and melted plastic into every crevice of your body.");
+                Console.WriteLine("Good job, you won the Darwin Award. Also congratulations, you've lost.");
+
+                }
+                if (response == "No")
+                {
+                Console.WriteLine("You walk away from the broken robot, which is still sputtering black fog and sparks. ");
+                Console.WriteLine("You walk down a corridor until you see a door. The door is creaked open, and a beam of light is shooting out from the   opening. ");
+                Console.WriteLine("You walk through the door, leaving this bizzare place behind.");
+                Console.WriteLine("Congratulations, you've won!");
+                }
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
